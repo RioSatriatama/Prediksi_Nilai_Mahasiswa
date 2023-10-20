7f66518cdea6608e8259770b019a0fd729233750
RioSatriatama <82066488+RioSatriatama@users.noreply.github.com>
2023-10-20 17:41:19 +0700

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        If TextBox1.Text >= 50 Then
+            TextBox2.Text = "Lulus"
+        Else
+            If TextBox1.Text < 50 Then
+                TextBox2.Text = "Tidak Lulus"
+            End If
+        End If
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+    End Sub
+End Class
